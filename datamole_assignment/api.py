524aa42fd6260928bf969f83fa41103770395724
Norbert Dopjera <ndopjera@gmail.com>
2025-02-12 19:15:41 +0100

    feat(api): repo events counts grouped by time (#6)

--- datamole_assignment/api.py
+++ datamole_assignment/api.py
@@ -1,11 +1,11 @@
 import statistics
-from datetime import timedelta
+from datetime import datetime, timezone, timedelta
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from starlette.responses import RedirectResponse
 
 from datamole_assignment.setup import services
-from datamole_assignment.service import GithubService, Event, EventType
+from datamole_assignment.service import GithubService, EventType
 
 
 events_router = APIRouter()
@@ -18,8 +18,8 @@ async def get_root() -> RedirectResponse:
     return RedirectResponse(url="swagger-ui.html")
 
 
-@events_router.get("/{owner}/{repo}/mean")
-async def events_mean(owner: str, repo: str, type: EventType = None) -> timedelta:
+@events_router.get("/{owner}/{repo}/mean_time")
+async def events_mean_time(owner: str, repo: str, type: EventType = None) -> timedelta:
     github: GithubService = services["github"]
     previous_time = None
     time_diffs = []
@@ -34,3 +34,21 @@ async def events_mean(owner: str, repo: str, type: EventType = None) -> timedelt
     if len(time_diffs) == 0:
         return timedelta(seconds=0)
     return timedelta(seconds=statistics.fmean(time_diffs))
+
+
+@events_router.get("/{owner}/{repo}/grouped")
+async def events_grouped(
+    owner: str, repo: str,
+    offset: int = Query(None, description="Max time offset in minutes")
+) -> dict[EventType, int]:
+
+    github: GithubService = services["github"]
+    now = datetime.now(timezone.utc)
+    groups = {}
+
+    async for event in github.events(owner, repo):
+        if offset is not None and event.created_at < (now - timedelta(minutes=offset)):
+            return groups
+        if event.type is not None:
+            groups[event.type] = groups.get(event.type, 0) + 1
+    return groups
