c587509e829a549197d41b42ba9c155f46f0631e
ndopj <ndopjera@gmail.com>
2025-02-12 17:08:37 +0100

    feat(setup): uvicorn server with configuration

--- datamole_assignment/main.py
+++ datamole_assignment/main.py
@@ -1,15 +1,30 @@
 from typing import Union
 
+import uvicorn
 from fastapi import FastAPI
+
+from datamole_assignment.setup import config, log
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    log.info(f"Github URL: {config.github_url}")
+    log.debug("This is a debug messages")
+    log.warning("This is a warning message")
+    log.error("This is an error message")
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == '__main__':
+    uvicorn.run("datamole_assignment.main:app",
+                port=config.port,
+                log_level=config.log_level.lower(),
+                reload=config.hot_reload,
+                log_config=config.log_config)
--- datamole_assignment/setup.py
+++ datamole_assignment/setup.py
@@ -1,3 +1,5 @@
+import logging
+
 from pydantic_settings import BaseSettings
 
 
@@ -5,6 +7,10 @@ class Settings(BaseSettings):
     port: int = 8080
     log_level: str = "debug"
     log_config: str = "dev_logger.yml"
+    hot_reload: bool = False
     github_url: str = "https://api.github.com"
 
 config = Settings()
+
+logging.basicConfig(level=config.log_level.upper())
+log = logging.getLogger("uvicorn.error")
